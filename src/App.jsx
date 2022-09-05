4e672c6c464f61a7f0b45accb3fcfa00750b2ada
jomit <metrotasiosg@gmail.com>
2022-09-05 14:57:29 +0300

    Made the headline and dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateTodo from "./components/CreateTodo";
+import TodoList from "./components/TodoList";
+import sun from "./assets/icon-sun.svg";
+import moon from "./assets/icon-moon.svg";
 
 function App() {
-  return <h1 className="text-center text-5xl mt-10">Hello World</h1>;
+  const [darkMode, setDarkMode] = useState(true);
+
+  const changeMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
+  function decideMode(dark, light) {
+    return darkMode ? dark : light;
+  }
+
+  return (
+    <div>
+      <div
+        className={`absolute z-10 w-screen h-2/5 bg-cover bg-no-repeat ${decideMode(
+          "bg-mobile-dark",
+          "bg-mobile-light"
+        )} ${decideMode("sm:bg-desktop-dark", "sm:bg-desktop-light")}`}
+      ></div>
+      <div
+        className={`flex justify-center h-screen ${decideMode(
+          "bg-veryDarkBlue",
+          "bg-veryLightGrayishBlue"
+        )}`}
+      >
+        <div className="w-screen z-20 mt-12 mx-5 md:mx-24 lg:mx-48 xl:mx-96 2xl:mx-[500px] 2xl:max-w-[700px]">
+          <div className="flex justify-between">
+            <h1 className="text-white text-3xl font-bold tracking-[0.4em] lg:text-5xl">
+              TODO
+            </h1>
+            <button>
+              <img
+                className="w-[26px] h-[26px]"
+                src={darkMode ? sun : moon}
+                alt=""
+                onClick={changeMode}
+              />
+            </button>
+          </div>
+          <CreateTodo />
+          <TodoList />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,47 @@ module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    backgroundImage: {
+      "desktop-dark": "url(./src/assets/bg-desktop-dark.jpg)",
+      "desktop-light": "url(./src/assets/bg-desktop-light.jpg)",
+      "mobile-dark": "url(./src/assets/bg-mobile-dark.jpg)",
+      "mobile-light": "url(./src/assets/bg-mobile-light.jpg)",
+    },
+    colors: {
+      // Defaults
+      white: "#ffffff",
+
+      // Primary
+      brightBlue: "hsl(220, 98%, 61%)",
+      checkBackground:
+        "linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)",
+
+      // Dark theme
+      veryDarkBlue: "hsl(235, 21%, 11%)",
+      veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+      lightGrayishBlue: "hsl(234, 39%, 85%)",
+      lightGrayishBlueHover: "hsl(236, 33%, 92%)",
+      darkGrayishBlue: "hsl(234, 11%, 52%)",
+      veryDarkGrayishBlue: "hsl(233, 14%, 35%)",
+      veryDarkGrayishBlue: "hsl(237, 14%, 26%)",
+
+      // Light theme
+      veryLightGray: "hsl(0, 0%, 98%)",
+      veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+      lightGrayishBlue: "hsl(233, 11%, 84%)",
+      darkGrayishBlue: "hsl(236, 9%, 61%)",
+      veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+    },
+    fontFamily: {
+      josefin: ["Josefin Sans", "sans-serif"],
+    },
   },
   plugins: [],
 };
